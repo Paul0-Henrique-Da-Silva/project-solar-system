4a1764bbfa43456410ea3178e208da3b1c825ddc
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-05-30 15:40:08 -0300

    solar-system-trybe

---
+++ src/components/Missions.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        <section className="list-missions">
+        {
+          missions.map((mission) => (
+            <MissionCard
+              key={ mission.name }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />
+          ))
+        }
+        </section>
+      </div>
+
+    );
+  }
+}
+
+export default Missions;
---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    return (
+      <div data-testid="planet-card" className="planet-card">
+        <p data-testid="planet-name" className="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+
+    );
+  }
+}
+
+export default PlanetCard;
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
